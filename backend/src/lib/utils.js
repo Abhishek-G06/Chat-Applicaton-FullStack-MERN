b99f6c673e90312a5aee957ddcee326df0af872e
Abhishek Gupta <80634185+Abhishek-G06@users.noreply.github.com>
2025-07-22 00:29:43 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -10,7 +10,7 @@ export const generateToken = (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 86400000,
         httpOnly: true,
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
         secure: process.env.NODE_ENV !== "development"
     });
 
